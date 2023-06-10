75ccd9e6d028bc738ac6ce6d5e52aab9fe5a4c5a
adenugbamichael <adenugbamicke@gmail.com>
2023-06-10 04:42:27 +0100

    Set up firebase

--- src/routes/home.component.jsx
+++ src/routes/home.component.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/jsx-key */
-// import { Outlet } from "react-router-dom"
+
 import Directory from "../components/directory/directory.component"
 
 const Home = () => {
@@ -36,7 +36,6 @@ const Home = () => {
     },
   ]
 
-  // <Outlet />
   return (
     <div>
       <Directory categories={categories} />
