51d0bd944cebdebb04df3978d3f174f74885ceed
adenugbamichael <adenugbamicke@gmail.com>
2023-06-11 20:26:49 +0100

    Product card component

--- src/assets/index.jsx
+++ src/assets/index.jsx
@@ -3,5 +3,29 @@ import mens from "./img/mens.jpg"
 import womens from "./img/womens.jpg"
 import hats from "./img/hats.jpg"
 import jackets from "./img/jackets.jpg"
+import redBrim from "./img/redBrim.jpg"
+import blueBeanie from "./img/blueBeanie.jpg"
+import brownCowboy from "./img/brownCowboy.jpg"
+import greyBrim from "./img/greyBrim.jpg"
+import greenBeanie from "./img/greenBeanie.jpg"
+import palmTreeCap from "./img/palmTreeCap.jpg"
+import redBeanie from "./img/redBeanie.jpg"
+import wolfCap from "./img/wolfCap.jpg"
+import blueSnapback from "./img/blueSnapback.jpg"
 
-export { sneakers, womens, hats, mens, jackets }
+export {
+  sneakers,
+  womens,
+  hats,
+  mens,
+  jackets,
+  redBrim,
+  blueBeanie,
+  brownCowboy,
+  greyBrim,
+  greenBeanie,
+  palmTreeCap,
+  redBeanie,
+  wolfCap,
+  blueSnapback,
+}
