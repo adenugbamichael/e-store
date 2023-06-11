4a8739057548d244e4b867253f9c16e00bd4d0e2
adenugbamichael <adenugbamicke@gmail.com>
2023-06-11 05:59:12 +0100

    Sign in form | Finishing authentication

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/jsx-key */
 
-import Directory from "../components/directory/directory.component"
+import Directory from "../../components/directory/directory.component"
 
 const Home = () => {
   const categories = [
