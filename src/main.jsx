1a6bab8d5fec71d390ec4146622eb40edde23043
adenugbamichael <adenugbamicke@gmail.com>
2023-06-12 08:10:41 +0100

    Cart icon and Dropdown | Toggle cart open

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,16 @@ import App from "./App.jsx"
 import "./index.scss"
 import { UserProvider } from "./contexts/user.context.jsx"
 import { ProductsProvider } from "./contexts/products.context.jsx"
+import { CartProvider } from "./contexts/cart.context.jsx"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
         <ProductsProvider>
-          <App />
+          <CartProvider>
+            <App />
+          </CartProvider>
         </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
