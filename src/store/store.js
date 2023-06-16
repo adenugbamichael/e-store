47674d15ecc1b8090f7e79ef237b0af81b7539a6
adenugbamichael <adenugbamicke@gmail.com>
2023-06-16 12:09:10 +0100

    Demystifying Middleware

--- src/store/store.js
+++ src/store/store.js
@@ -3,11 +3,25 @@ import {
   legacy_createStore as createStore,
   applyMiddleware,
 } from "redux"
-import logger from "redux-logger"
+// import logger from "redux-logger"
 import { rootReducer } from "./root-reducer"
 
-const middlewares = [logger]
+const loggerMiddleware = (store) => (next) => (action) => {
+  if (action.type) {
+    return next(action)
+  }
 
-const composedEnchancers = compose(applyMiddleware(...middlewares))
+  console.log("type: ", action.type)
+  console.log("payload: ", action.payload)
+  console.log("currentState: ", store.getState())
 
-export const store = createStore(rootReducer, undefined, composedEnchancers)
+  next(action)
+
+  console.log("next state: ", store.getState())
+}
+
+const middleWares = [loggerMiddleware]
+
+const composedEnhancers = compose(applyMiddleware(...middleWares))
+
+export const store = createStore(rootReducer, undefined, composedEnhancers)
